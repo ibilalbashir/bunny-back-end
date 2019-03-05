2324bb75f01ac4aed0ca03885d0b954a010245d4
ibilalbashir <ibilalbashir@gmail.com>
2019-03-05 18:55:03 +0500

    generate coupon

--- common/models/merchant.js
+++ common/models/merchant.js
@@ -1,5 +1,25 @@
 'use strict';
+var app = require('../../server/server')
+var randomstring = require("randomstring");
 
-module.exports = function(Merchant) {
+module.exports = function (merchant) {
+
+  merchant.prototype.generateCoupon = function (callback) {
+    var couponCode = randomstring.generate(7);
+    var coupon = app.models.coupon;
+    var m = this;
+    coupon.create({
+      'code': couponCode,
+      'isUsed': false,
+      'merchantId': m.id
+    }, function (err, succ) {
+      if (err) {
+        console.log(err);
+      }
+      console.log(succ);
+    })
+    // TODO
+    callback(null, couponCode);
+  };
 
 };
